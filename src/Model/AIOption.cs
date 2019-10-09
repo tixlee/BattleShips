fb5022941d8bb3788ff907c6a7c6ce5e5a4dff63
Jason Goh <jasongohbl@gmail.com>
2019-10-09 18:02:43 +0800

    add namespace BattleShips

--- src/Model/AIOption.cs
+++ src/Model/AIOption.cs
@@ -3,27 +3,30 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-// using System.Data;
 using System.Diagnostics;
-/// <summary>
-/// The different AI levels.
-/// </summary>
-public enum AIOption
+namespace BattleShips
 {
-	/// <summary>
-	/// Easy, total random shooting
-	/// </summary>
-	Easy,
 
 	/// <summary>
-	/// Medium, marks squares around hits
+	/// The different AI levels.
 	/// </summary>
-	Medium,
+	public enum AIOption
+	{
+		/// <summary>
+		/// Easy, total random shooting
+		/// </summary>
+		Easy,
 
-	/// <summary>
-	/// As medium, but removes shots once it misses
-	/// </summary>
-	Hard
+		/// <summary>
+		/// Medium, marks squares around hits
+		/// </summary>
+		Medium,
+
+		/// <summary>
+		/// As medium, but removes shots once it misses
+		/// </summary>
+		Hard
+	}
 }
 
 //=======================================================
--- src/Model/Direction.cs
+++ src/Model/Direction.cs
@@ -3,22 +3,24 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-// using System.Data;
 using System.Diagnostics;
-/// <summary>
-/// The direction the ship can be oriented.
-/// </summary>
-public enum Direction
+namespace BattleShips
 {
 	/// <summary>
-	/// The ship is oriented left/right
+	/// The direction the ship can be oriented.
 	/// </summary>
-	LeftRight,
+	public enum Direction
+	{
+		/// <summary>
+		/// The ship is oriented left/right
+		/// </summary>
+		LeftRight,
 
-	/// <summary>
-	/// The ship is oriented up/down
-	/// </summary>
-	UpDown
+		/// <summary>
+		/// The ship is oriented up/down
+		/// </summary>
+		UpDown
+	}
 }
 
 //=======================================================
