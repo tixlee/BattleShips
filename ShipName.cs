9780aeb04b7d95c3f224c3494374002cf031211e
Lee Jia Jun <101210855@students.swinburne.edu.my>
2019-10-09 17:02:36 +0800

    add new Class Ship and new Enumeration ShipName

---
+++ ShipName.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BattleShips
+{
+	public class ShipName
+	{
+		public ShipName ()
+		{
+		}
+	}
+}
---
+++ Tile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BattleShips
+{
+	public class Tile
+	{
+		public Tile ()
+		{
+		}
+	}
+}
